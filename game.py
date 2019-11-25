e346a7326368108a2529ffcf92fb57183cb70eef
Carlos Moreno Morera <carmor06@ucm.es>
2019-11-25 10:23:25 +0100

    Implementing distribute character function

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ Created on Sun Nov 24 17:04:53 2019
 from __future__ import print_function
 from enum import Enum
 from player import Player
+import random
 
 State = Enum('State', 'Creation Ask Vote Guess Final')
 
@@ -28,11 +29,27 @@ class Game:
         self.__players = {}
     
     def add_player(self, id):
+        """
+        Adds a player to the game.
+        
+        Returns
+        -------
+        None.
+        
+        """
         if (self.__state == State.Creation and not(id in self.__players)):
             self.__players[id] = Player()
             self.__turn.append(id)
             
     def remove_player(self, id):
+        """
+        Removes a player from the game.
+        
+        Returns
+        -------
+        None.
+        
+        """
         if (id in self.__players):
             del self.__players[id]
             
@@ -45,3 +62,27 @@ class Game:
         int: number of players in the game.
         """
         return len(self.__players)
+    
+    def distribute_characters(self):
+        received = [False for i in range(self.get_num_players())]
+        num_given = 0
+        
+        for i in range(self.get_num_players() - 2):
+            shift = random.randint(1, self.get_num_players() - num_given - 1)
+            j = i
+            
+            while (shift > 0):
+                if j < self.get_num_players() - 1:
+                    j += 1
+                else:
+                    j = 0
+                
+                if not(received[j]) and j != i:
+                    shift -= 1
+                    
+            self.__players[self.__turn[j]].set_a_character(self.__turn[i])
+            num_given += 1
+            received[j] = True
+            
+        #No terminada, falta repartir a los últimos dos jugadores
+            
