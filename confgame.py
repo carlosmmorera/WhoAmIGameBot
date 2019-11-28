ee0515bebda5c255012dd61c248c310a769aa9ae
Carlos Moreno Morera <carmor06@ucm.es>
2019-11-28 20:46:00 +0100

    Implementada la gestión de la votación

---
+++ confgame.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 19:47:22 2019
+
+@author: Carlos Moreno Morera
+"""
+from enum import Enum
+
+State = Enum('State', 'Creation Distribution TurnDetermination Ask Vote Guess Final')
+
+# Minimum number of players needed to start a game.
+MIN_NUM_PLAYERS = 2
+# Initialization of a new votation
+INIT_VOTES = {'Yes': 0, 'No': 0}
+# Duration (in seconds) of a votation
+VOTATION_TIME = 60
--- game.py
+++ game.py
@@ -6,14 +6,35 @@ Created on Sun Nov 24 17:04:53 2019
 """
 
 from __future__ import print_function
-from enum import Enum
 from player import Player
+import confgame as cfg
 import random
-
-State = Enum('State', 'Creation Ask Vote Guess Final')
+import time
 
 class Game:
+    """
+    Game class implements necessary methods to manage a Who Am I game.
     
+    Attributes
+    ----------
+    __num_turn: int
+        Number of the actual turn.
+    __turn: list
+        List of players identifier which determines next player turn.
+    __first_player: int
+        Indentifier of the player who started playing this game.
+    __state: cfg.State
+        State of the game.
+    __players: dict
+        Dictionary of players in this game.
+    __init_time: float
+        Initial time of the last votation.
+    __question: str
+        Question of the last votation.
+    __votes: dict
+        Dictionary which stores the votation results.
+    
+    """
     def __init__(self):
         """
         Class Constructor.
@@ -25,8 +46,12 @@ class Game:
         """
         self.__num_turn = 0
         self.__turn = []
-        self.__state = State.Creation
+        self.__first_player = None
+        self.__state = cfg.State.Creation
         self.__players = {}
+        self.__init_time = None
+        self.__question = ""
+        self.__votes = cfg.INIT_VOTES
     
     def add_player(self, id):
         """
@@ -37,7 +62,7 @@ class Game:
         None.
         
         """
-        if (self.__state == State.Creation and not(id in self.__players)):
+        if (self.__state == cfg.State.Creation and not(id in self.__players)):
             self.__players[id] = Player()
             self.__turn.append(id)
             
@@ -50,7 +75,7 @@ class Game:
         None.
         
         """
-        if (id in self.__players):
+        if (self.__state == cfg.State.Final and id in self.__players):
             del self.__players[id]
             
     def get_num_players(self):
@@ -113,41 +138,98 @@ class Game:
         -------
         None.
         """
-        received = [False for i in range(self.get_num_players())]
-        num_given = 0
-        
-        for i in range(self.get_num_players() - 2):
-            shift = random.randint(1, self.get_num_players() - num_given - 1)
-            j = i
+        if self.__state == cfg.State.Distribution:
+            received = [False for i in range(self.get_num_players())]
+            num_given = 0
             
-            while (shift > 0):
-                if j < self.get_num_players() - 1:
-                    j += 1
-                else:
-                    j = 0
+            for i in range(self.get_num_players() - 2):
+                shift = random.randint(1, self.get_num_players() - num_given - 1)
+                j = i
                 
-                if not(received[j]) and j != i:
-                    shift -= 1
+                while (shift > 0):
+                    if j < self.get_num_players() - 1:
+                        j += 1
+                    else:
+                        j = 0
+                    
+                    if not(received[j]) and j != i:
+                        shift -= 1
+                
+                self.__set_char(i, j, received)
+                num_given += 1
             
-            self.__set_char(i, j, received)
-            num_given += 1
-        
-        sec_last = self.get_num_players() - 2
-        last = self.get_num_players() - 1
-        
-        if not(received[sec_last]):
-            if received[last]:
-                self.__set_char(last, sec_last, received)
-                self.__set_char(sec_last, self.__first_false(received), received)
-            else:
-                self.__set_char(last, sec_last, received)
+            sec_last = self.get_num_players() - 2
+            last = self.get_num_players() - 1
+            
+            if not(received[sec_last]):
+                if received[last]:
+                    self.__set_char(last, sec_last, received)
+                    self.__set_char(sec_last, self.__first_false(received), received)
+                else:
+                    self.__set_char(last, sec_last, received)
+                    self.__set_char(sec_last, last, received)
+            elif not(received[last]):
                 self.__set_char(sec_last, last, received)
-        elif not(received[last]):
-            self.__set_char(sec_last, last, received)
-            self.__set_char(last, self.__first_false(received), received)
-        else:
-            l = [sec_last, last]
-            random.shuffle(l)
-            self.__set_char(l[0], self.__first_false(received), received)
-            self.__set_char(l[1], self.__first_false(received), received)
+                self.__set_char(last, self.__first_false(received), received)
+            else:
+                l = [sec_last, last]
+                random.shuffle(l)
+                self.__set_char(l[0], self.__first_false(received), received)
+                self.__set_char(l[1], self.__first_false(received), received)
+                
+            self.__state = cfg.State.TurnDetermination
             
+    def start_votation(self, q):
+        """
+        Starts the votation phase.
+        
+        Returns
+        -------
+        None.
+        """
+        if self.__state == cfg.State.Vote:
+            self.__votes = cfg.INIT_VOTES
+            self.__question = q
+            self.__init_time = time.time()
+    
+    def vote_yes(self):
+        """
+        Adds an affirmative vote to the votation in process.
+        
+        Returns
+        -------
+        None.
+        """
+        if self.__state == cfg.State.Vote:
+            self.__votes['Yes'] += 1
+            
+    def vote_no(self):
+        """
+        Adds an negative vote to the votation in process.
+        
+        Returns
+        -------
+        None.
+        """
+        if self.__state == cfg.State.Vote:
+            self.__votes['No'] += 1
+            
+    def has_votation_finished(self):
+        """
+        Checks if the votation state has finished.
+        
+        Returns
+        -------
+        bool: True if any of these conditions is true:
+            - The state of the game is different than 'Vote' state.
+            - Every player has voted.
+            - The time of the votation has reached the maximum number of
+              seconds.
+            False in other case.
+        """
+        if self.__state != cfg.State.Vote:
+            return True
+        
+        t = time.time() - self.__init_time
+        numvotes = self.__votes['Yes'] + self.__votes['No']
+        return self.get_num_players() == numvotes or t >= cfg.VOTATION_TIME
